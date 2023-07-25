925d22f696e18a43ab01d347fe59f9628c38a383
Pascal Brissette <pascal.brissette@mcgill.ca>
2023-07-25 13:20:56 -0400

    Ajout chemin dans argument importTable

--- R/importTable.R
+++ R/importTable.R
@@ -1,26 +1,26 @@
-library(data.table)
-library(readxl)
 
 #### Importation d'une table simple. Si le fichier importé est de type .xlsx, la table sera transformée en data.table
-importTable_f <- function() {
+importTable_f <- function(chemin) {
+  library(data.table)
+  library(readxl)
   # Demander le chemin du fichier
-  reponse <- readline("Importation des données. Fournissez le lien exact du fichier de données, sans guillemets:  ")
+  # reponse <- readline("Importation des données. Fournissez le lien exact du fichier de données, sans guillemets:  ")
 
   # Vérifier si le chemin conduit au fichier
-  if(!file.exists(reponse)) {
+  if(!file.exists(chemin)) {
     stop("Vérifier le chemin du fichier")
   }
 
   # Vérifier si la table est dans un format approprié
-  if(!grepl(pattern = "([ct]sv$)|(xlsx$)", reponse)) {
+  if(!grepl(pattern = "([ct]sv$)|(xlsx$)", chemin)) {
     stop("Le fichier doit avoir une extension .xlsx, .tsv ou .csv")
   }
 
   # Importation de la table brute
-  if(!grepl(pattern = "xlsx$", reponse)) {
-    x <- fread(reponse)
+  if(!grepl(pattern = "xlsx$", chemin)) {
+    x <- fread(chemin)
   } else {
-    x <- read_excel(reponse, sheet = 1) |> setDT()
+    x <- read_excel(chemin, sheet = 1) |> setDT()
   }
   return(x)
 }
